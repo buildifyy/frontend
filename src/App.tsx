947285768912d048ac2aeaf903fdfad4335f9351
Tak1za <varungupta2015135@gmail.com>
2023-04-29 20:40:54 +0530

    added plus button and fixed undefined version by going back to react 17

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,61 @@
+import { CloseOutlined, PlusCircleOutlined } from "@ant-design/icons";
+import { Button, Divider } from "antd";
 import { GraphinData, Utils } from "@antv/graphin";
 import { Graph } from "./components/graph/Graph";
-import { useState } from "react";
-import { CloseOutlined } from "@ant-design/icons";
-import { Divider } from "antd";
+import { useEffect, useState } from "react";
 
 const App = () => {
   const [selectedNode, setSelectedNode] = useState<string | undefined>();
-  const [initData] = useState(Utils.mock(30).tree().graphin());
-
-  const transformData = (): GraphinData => {
-    const transformedData: GraphinData = { nodes: [], edges: [] };
-
-    transformedData.nodes = initData.nodes.map((node) => {
-      return {
-        ...node,
-        style: {
-          ...node.style,
-          keyshape: {
-            ...node.style?.keyshape,
-            size: 30,
-          },
-          label: {
-            ...node.style?.label,
-            offset: [0, 20],
-          },
-        },
-      };
-    });
-
-    transformedData.edges = [...initData.edges];
-
-    return transformedData;
-  };
+  const [initData, setInitData] = useState(Utils.mock(30).tree().graphin());
 
   const handleDeselectNode = () => {
     setSelectedNode(undefined);
   };
 
+  useEffect(() => {
+    const transformData = () => {
+      const transformedData: GraphinData = { nodes: [], edges: [] };
+
+      transformedData.nodes = initData.nodes.map((node) => {
+        return {
+          ...node,
+          style: {
+            ...node.style,
+            keyshape: {
+              ...node.style?.keyshape,
+              size: 30,
+            },
+            label: {
+              ...node.style?.label,
+              offset: [0, 20],
+            },
+          },
+        };
+      });
+
+      transformedData.edges = [...initData.edges];
+
+      setInitData(transformedData);
+    };
+
+    transformData();
+  }, [initData]);
+
   return (
     <>
+      <Graph
+        data={initData ? initData : { nodes: [], edges: [] }}
+        setSelectedNode={setSelectedNode}
+        handleDeselectNode={handleDeselectNode}
+      />
       {selectedNode ? (
-        <div className="absolute top-5 right-5 h-[calc(100%-40px)] text-white z-50 w-80 p-2 rounded-md bg-popup overflow-y-auto">
+        <div className="absolute top-20 right-5 h-[calc(100%-100px)] text-white z-50 w-80 p-2 rounded-md bg-popup">
           <div className="flex justify-between items-center">
             <p className="text-lg">Selected Node</p>
             <CloseOutlined onClick={() => setSelectedNode(undefined)} />
           </div>
-          <Divider className="my-2 bg-[#1f1f1f]"/>
-          <div className="flex flex-col gap-2">
+          <Divider className="my-2 bg-[#1f1f1f]" />
+          <div className="flex flex-col gap-2  overflow-y-auto h-[calc(100%-50px)]">
             Node Details
             <div className="rounded-md bg-[#1f1f1f] p-2 whitespace-pre">
               {JSON.stringify(
@@ -74,11 +83,16 @@ const App = () => {
           </div>
         </div>
       ) : null}
-      <Graph
-        data={transformData()}
-        setSelectedNode={setSelectedNode}
-        handleDeselectNode={handleDeselectNode}
-      />
+      <div className="absolute top-5 right-5">
+        <Button
+          type="primary"
+          icon={<PlusCircleOutlined />}
+          size={"large"}
+          className="flex items-center bg-popup"
+        >
+          Add
+        </Button>
+      </div>
     </>
   );
 };
